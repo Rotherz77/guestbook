c5205122e0fd99dea9058652e9c3115787b7f3c9
Steven Rotherham <rotherz77@outlook.com>
2025-07-01 21:17:11 +0930

    Feat: Updated app.py code

---
+++ Desktop/Full Stack Course/Python Projects/Guest Book/app.py
@@ -0,0 +1,37 @@
+from flask import Flask, request, jsonify
+import json
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return '<h1>The backend is working!</h1>'
+
+DATA_FILE = "messages.json"
+
+def load_messages():
+    if os.path.exists(DATA_FILE):
+        with open(DATA_FILE, "r") as f:
+            return json.load(f)
+    else:
+        return []
+
+@app.route('/messages', methods=["GET"])
+def get_messages():
+    messages = load_messages()
+    return jsonify(messages)
+
+@app.route('/messages', methods=["POST"])
+def add_messages():
+    data = request.get_json()
+    messages = load_messages()
+    messages.append(data)
+    
+    with open(DATA_FILE, "w") as f:
+        json.dump(messages, f, indent=2)
+    
+    return jsonify({"success": True})
+
+if __name__ == '__main__':
+    app.run(debug=True)
